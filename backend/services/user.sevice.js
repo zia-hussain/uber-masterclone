66ecd671eae04e31d31e4d4b09dda86557a40501
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-26 22:30:00 +0500

    authentication done !!

---
+++ backend/services/user.sevice.js
@@ -0,0 +1,22 @@
+const userModel = require("../models/user.model");
+
+module.exports.createUser = async ({
+  firstname,
+  lastname,
+  email,
+  password,
+}) => {
+  if (!firstname || !email || !password) {
+    throw new Error("All fields are required");
+  }
+  const user = userModel.create({
+    fullname: {
+      firstname,
+      lastname,
+    },
+    email,
+    password,
+  });
+
+  return user;
+};
