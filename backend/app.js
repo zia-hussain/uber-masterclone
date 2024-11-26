e99b2fd01fba4f0a50df6954d78c2f64fd5885ef
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-26 22:29:43 +0500

    authentication done !!

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,15 @@ dotenv.config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
