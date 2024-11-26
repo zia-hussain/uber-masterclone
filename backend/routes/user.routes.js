3ddd0f8054066faa9bc9485d221a437a6a4adb9d
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-26 22:29:58 +0500

    authentication done !!

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const userController = require("../controllers/user.controller");
+
+const router = express.Router();
+const { body } = require("express-validator");
+
+router.post(
+  "/register",
+  [
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First name must be at least 3 characters long"),
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters"),
+  ],
+  userController.registerUser
+);
+
+module.exports = router;
