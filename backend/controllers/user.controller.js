e1ecbd760e4d23af8d0a5cc7714cd79879f59a43
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-27 21:15:01 +0500

    Connected to mongo db !!

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,16 +2,21 @@ const userModel = require("../models/user.model");
 const userService = require("../services/user.sevice");
 const { validationResult } = require("express-validator");
 
-module.exports.registerUser = async function (req, res, next) {
-  console.log("Incoming Request Body:", req.body);
+module.exports.registerUser = async (req, res, next) => {
+  console.log("REquest dot body", req.body);
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors.array());
     return res.status(400).json({ errors: errors.array() });
   }
 
   const { fullname, email, password } = req.body;
+
+  const isUserAlready = await userModel.findOne({ email });
+
+  if (isUserAlready) {
+    return res.status(400).json({ message: "User already exist" });
+  }
 
   const hashedPassword = await userModel.hashPassword(password);
 
