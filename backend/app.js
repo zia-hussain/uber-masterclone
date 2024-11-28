858dd7c1b1b7a768329a8053b32c70def6feb0d2
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-28 20:58:18 +0500

    profile setup almost done !!!

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 
@@ -11,6 +12,7 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
