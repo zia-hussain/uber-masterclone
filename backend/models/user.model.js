831e71632009cf30cd617a3011239bcd3515a347
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-30 17:57:05 +0500

    register for captain complete !!!

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -7,11 +7,11 @@ const userSchema = new mongoose.Schema({
     firstname: {
       type: String,
       required: true,
-      minlength: [3, "First name must be at least 3 characters long"],
+      minlength: [3, "Firstname must be at least 3 characters long"],
     },
     lastname: {
       type: String,
-      minlength: [3, "Last name must be at least 3 characters long"],
+      minlength: [3, "Lastname must be at least 3 characters long"],
     },
   },
   email: {
