129f31b1798ac1fdd54a0382946d4b68ae1ef357
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-30 14:40:11 +0500

    user authentication done !!!

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,8 +3,15 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 module.exports.authUser = async (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  const isBlackListed = await userModel.findOne({ token: token });
+
+  if (isBlackListed) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await userModel.findById(decoded._id);
