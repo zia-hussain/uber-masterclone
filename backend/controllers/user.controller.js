13901e268c735e2070e8e2c2835693a29ed1ac35
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-30 14:40:08 +0500

    user authentication done !!!

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model");
 const userService = require("../services/user.sevice");
 const { validationResult } = require("express-validator");
+const blacklistToken = require("../models/blacklistToken.model");
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
@@ -59,4 +60,12 @@ module.exports.loginUser = async (req, res, next) => {
 
 module.exports.getUserProfile = async (req, res, next) => {
   res.status(200).json(req.user);
+};
+
+module.exports.logoutUser = async (req, res, next) => {
+  res.clearCookie("token");
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  await blacklistToken.create({ token });
+  res.status(200).json({ message: "Logged out successfully" });
 };
