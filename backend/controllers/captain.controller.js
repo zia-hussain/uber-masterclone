d3529fb591f2ffdea4f22c7472f7863d00695db3
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-30 17:56:57 +0500

    register for captain complete !!!

---
+++ backend/controllers/captain.controller.js
@@ -0,0 +1,35 @@
+const captainModel = require("../models/captain.model");
+const captainService = require("../services/captain.service");
+const { validationResult } = require("express-validator");
+
+module.exports.registerCaptain = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { fullname, email, password, vehicle } = req.body;
+
+  const isCaptainAlreadyExist = await captainModel.findOne({ email });
+
+  if (isCaptainAlreadyExist) {
+    return res.status(400).json({ message: "Captain already exist" });
+  }
+
+  const hashedPassword = await captainModel.hashPassword(password);
+
+  const captain = await captainService.createCaptain({
+    firstname: fullname.firstname,
+    lastname: fullname.lastname,
+    email,
+    password: hashedPassword,
+    color: vehicle.color,
+    plate: vehicle.plate,
+    capacity: vehicle.capacity,
+    vehicleType: vehicle.vehicleType,
+  });
+
+  const token = captain.generateAuthToken();
+
+  res.status(201).json({ token, captain });
+};
