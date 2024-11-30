8caeed781aaf75097293f8d9a90fa5860680d291
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-30 21:50:36 +0500

    Sign in complete !

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
   return (
     <div>
       <div
-        className="h-screen pt-2 w-full bg-red-400 flex justify-between flex-col bg-cover bg-center"
+        className="h-screen pt-2 w-full bg-gray-300  flex justify-between flex-col bg-cover bg-center"
         style={{ backgroundImage: `url(${Background})` }}
       >
-        <img className="w-32 ml-4" src={Logo} alt="" />
+        <img className="w-24 ml-4" src={Logo} alt="" />
         <div className="bg-white pb-5 py-4 px-4">
           <h2 className="text-3xl font-bold">Get Started with Uber</h2>
           <Link
