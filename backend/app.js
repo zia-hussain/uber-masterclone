2bb8c61848e14223c593705fe3b2ab7850d6d650
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-30 17:56:55 +0500

    register for captain complete !!!

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 
 connectToDb();
 
@@ -18,5 +19,6 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 app.use("/users", userRoutes);
+app.use("/captain", captainRoutes);
 
 module.exports = app;
