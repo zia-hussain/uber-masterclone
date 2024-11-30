7aaca5e63ec67d818f5131aa89d73a456de20581
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-30 21:27:15 +0500

    Login page done !!

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainSignup from "./pages/CaptainSignup";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-logsignupin" element={<CaptainSignup />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
