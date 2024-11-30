1d93c582a5501564259980a8cf55883c1e275f9b
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-30 17:57:00 +0500

    register for captain complete !!!

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -10,6 +10,10 @@ module.exports.registerUser = async (req, res, next) => {
   }
 
   const { fullname, email, password } = req.body;
+  const isUserAlreadyExist = await userModel.findOne({ email });
+  if (isUserAlreadyExist) {
+    return res.status(400).json({ message: "User already exist" });
+  }
 
   const isUserAlready = await userModel.findOne({ email });
 
