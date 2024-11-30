311971abe1c36a23ce8a3bba8847b92b0c68242e
zia-hussain <syedziashahgillani76@gmail.com>
2024-12-01 00:37:36 +0500

    UserLogin page complete !!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import UserLogin from "./pages/UserLogin";
 import UserSignup from "./pages/UserSignup";
 import CaptainLogin from "./pages/CaptainLogin";
 import CaptainSignup from "./pages/CaptainSignup";
+import UnsupportedScreenModal from "./components/UnsupportedScreenModal";
 
 const App = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    // Add resize event listener
+    window.addEventListener("resize", handleResize);
+
+    // Clean up listener on unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<UserLogin />} />
-        <Route path="/signup" element={<UserSignup />} />
-        <Route path="/captain-login" element={<CaptainLogin />} />
-        <Route path="/captain-logsignupin" element={<CaptainSignup />} />
-      </Routes>
+      {/* Modal for non-mobile screens */}
+      {!isMobile && <UnsupportedScreenModal />}
+
+      {/* App Routes */}
+      <div className={`${!isMobile ? "overflow-hidden h-screen" : ""}`}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<UserLogin />} />
+          <Route path="/signup" element={<UserSignup />} />
+          <Route path="/captain-login" element={<CaptainLogin />} />
+          <Route path="/captain-signup" element={<CaptainSignup />} />
+        </Routes>
+      </div>
     </div>
   );
 };
