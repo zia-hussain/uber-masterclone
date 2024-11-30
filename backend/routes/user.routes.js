8141787d72bd231f633b9f9717bd8a8ebcd6e637
zia-hussain <syedziashahgillani76@gmail.com>
2024-11-30 14:40:17 +0500

    user authentication done !!!

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -31,5 +31,6 @@ router.post(
 );
 
 router.get("/profile", authMiddleware.authUser, userController.getUserProfile);
+router.get("/logout", authMiddleware.authUser, userController.logoutUser);
 
 module.exports = router;
