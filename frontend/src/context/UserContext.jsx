5a490704e04c2ac6f71895674b9a0b6e2ba24857
zia-hussain <syedziashahgillani76@gmail.com>
2024-12-27 19:53:27 +0500

    working !!!

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from "react";
+
+export const UserDataContext = createContext();
+
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState({
+    email: "",
+    fullName: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+
+  return (
+    <UserDataContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserDataContext.Provider>
+  );
+};
+
+export default UserContext;
