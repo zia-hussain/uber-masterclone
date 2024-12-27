fa3346f8f604c44dced3041a2a27c1bc99d9760b
zia-hussain <syedziashahgillani76@gmail.com>
2024-12-27 19:53:33 +0500

    working !!!

---
+++ frontend/src/pages/Start.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Logo from "../../public/Logo.svg";
+import Background from "../assets/homeBackground.jpg";
+import { HiChevronRight } from "react-icons/hi";
+import { Link } from "react-router-dom";
+
+const Start = () => {
+  return (
+    <div>
+      <div
+        className="h-screen pt-2 w-full bg-gray-300  flex justify-between flex-col bg-cover bg-center"
+        style={{ backgroundImage: `url(${Background})` }}
+      >
+        <img className="w-24 ml-4" src={Logo} alt="" />
+        <div className="bg-white pb-5 py-4 px-4">
+          <h2 className="text-3xl font-bold">Get Started with Uber</h2>
+          <Link
+            to={"/login"}
+            className="w-full bg-black text-white font-bold  py-3 rounded mt-6 flex items-center justify-center relative hover:bg-gray-800 transition"
+          >
+            <span>Continue</span>
+            <span className="absolute right-4">
+              <HiChevronRight className="text-white w-7 h-7" />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
