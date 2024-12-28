e355f9ae18db0483fc52a05af40f53938d9273bd
zia-hussain <syedziashahgillani76@gmail.com>
2024-12-28 23:21:39 +0500

    userAuthentication complete !!!

---
+++ frontend/src/pages/UserLogout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const UserLogout = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  axios
+    .get(`${import.meta.env.VITE_API_URL}/users/logout`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => {
+      navigate("/login");
+    });
+
+  return <div>UserLogout</div>;
+};
+
+export default UserLogout;
