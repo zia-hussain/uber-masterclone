41147513a82579f1a88269fcfb346293a8692062
zia-hussain <syedziashahgillani76@gmail.com>
2024-12-31 16:59:13 +0500

    working ...!

--- frontend/src/pages/CaptainProtectWrapper.jsx
+++ frontend/src/pages/CaptainProtectWrapper.jsx
@@ -1,17 +1,40 @@
-import React, { useContext, useEffect } from "react";
-import { CaptainDataContext } from "../context/CaptainContext";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { CaptainDataContext } from "../context/CaptainContext";
 
 const CaptainProtectWrapper = ({ children }) => {
-  const navigate = useNavigate();
   const token = localStorage.getItem("token");
-  const { captain, setCaptain } = React.useContext(CaptainDataContext);
+  const navigate = useNavigate();
+  const { captain, setCaptain } = useContext(CaptainDataContext);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!token) {
       navigate("/captain-login");
     }
+
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/captains/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setCaptain(response.data.captain);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        localStorage.removeItem("token");
+        navigate("/captain-login");
+      });
   }, [token]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return <>{children}</>;
 };
