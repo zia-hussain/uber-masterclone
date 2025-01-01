aec236736d83808e9d6f5166062e1ae2fe7c8953
zia-hussain <syedziashahgillani76@gmail.com>
2025-01-01 17:42:53 +0500

    authentication complete !!!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import UserLogin from "./pages/UserLogin";
 import UserSignup from "./pages/UserSignup";
