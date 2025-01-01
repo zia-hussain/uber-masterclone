520f07a5d59267f0cf37dce1eeaec4b21cf14f3d
zia-hussain <syedziashahgillani76@gmail.com>
2025-01-01 18:14:00 +0500

    home ui start for user

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Logo from "../../public/Logo.svg";
+import { FaUserCircle } from "react-icons/fa";
+import Map from "../assets/map.gif";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <header className="flex justify-between items-center py-4 px-4 relative z-10">
+        <img src={Logo} alt="Uber Logo" className="w-24" />
+        <button className="flex items-center justify-center p-3 bg-black text-white rounded-full shadow-md hover:bg-gray-800 focus:ring-2 focus:ring-gray-400 transition-all">
+          <FaUserCircle size={24} />
+        </button>
+      </header>
+
+      <div className="absolute inset-0 h-screen w-screen z-0">
+        <img className="h-full w-full object-cover" src={Map} alt="" />
+      </div>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default Home;
