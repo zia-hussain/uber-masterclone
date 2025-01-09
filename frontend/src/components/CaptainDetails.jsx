af9788136d066961f9c27c620ef798d0452253aa
zia-hussain <syedziashahgillani76@gmail.com>
2025-01-09 14:13:56 +0500

    captain ui started !!!

---
+++ frontend/src/components/CaptainDetails.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const CaptainDetails = () => {
+  return (
+    <div className="h-full">
+      <div
+        className={`absolute top-3 left-1/2 transform -translate-x-1/2 w-14 h-1 bg-gray-300 rounded-full mb-4 transition-all duration-300
+  `}
+      ></div>
+      <div className="p-6 h-full flex flex-col justify-around">
+        {/* Profile Header */}
+        <div className="flex items-center justify-between mb-6 h-14">
+          <div className="flex items-center gap-3">
+            <div className="h-12 w-12 rounded-full overflow-hidden ]">
+              <img
+                src="https://img.freepik.com/free-photo/young-adult-man-wearing-hoodie-beanie_23-2149393636.jpg"
+                alt="Captain"
+                className="h-full w-full object-cover"
+              />
+            </div>
+            <div>
+              <h2 className="font-semibold">Jeremiah Curtis</h2>
+              <p className="text-sm text-gray-500">Basic level</p>
+            </div>
+          </div>
+          <div className="text-right">
+            <p className="font-semibold">$325.00</p>
+            <p className="text-sm text-gray-500">Earned</p>
+          </div>
+        </div>
+
+        {/* Stats Card */}
+        <div className="bg-[#FFD60A] rounded-lg p-4 w-full mb-6 h-36">
+          <div className="flex items-center justify-center h-full gap-4">
+            <div className="text-center ">
+              <div className="flex flex-col items-center justify-center gap-1">
+                <i className="text-3xl text-gray-600 font-thin ri-time-line"></i>
+                <span className="font-semibold">10.2</span>
+              </div>
+              <p className="text-xs text-gray-600 mt-1">HOURS ONLINE</p>
+            </div>
+            <div className="text-center">
+              <div className="flex flex-col items-center justify-center gap-1">
+                <i className="text-3xl text-gray-600 font-thin ri-speed-up-line"></i>
+                <span className="font-semibold">30 KM</span>
+              </div>
+              <p className="text-xs text-gray-600 mt-1">TOTAL DISTANCE</p>
+            </div>
+            <div className="text-center">
+              <div className="flex flex-col items-center justify-center gap-1">
+                <i className="text-3xl text-gray-600 font-thin ri-file-list-3-line"></i>
+                <span className="font-semibold">20</span>
+              </div>
+              <p className="text-xs text-gray-600 mt-1">TOTAL JOBS</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainDetails;
