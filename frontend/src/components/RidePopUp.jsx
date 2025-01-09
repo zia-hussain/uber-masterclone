f9439f3d2ff5ab524cb5a7f9963acefb849e5c90
zia-hussain <syedziashahgillani76@gmail.com>
2025-01-09 18:29:08 +0500

    little bit changes !!!

--- frontend/src/components/RidePopUp.jsx
+++ frontend/src/components/RidePopUp.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const RidePopUp = () => {
-  return (
-    <div>
-      <h1>Pop Up</h1>
-    </div>
-  );
+  return <div>{/* <h1>Pop Up</h1> */}</div>;
 };
 
 export default RidePopUp;
