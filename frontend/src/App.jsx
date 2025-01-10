0dbe67880b7e4d3c5ada77ec29ee28f249c7b443
zia-hussain <syedziashahgillani76@gmail.com>
2025-01-10 20:50:44 +0500

    captain almost complete !!!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import CaptainHome from "./pages/CaptainHome";
 import CaptainProtectWrapper from "./pages/CaptainProtectWrapper";
 import Riding from "./pages/Riding";
 import "remixicon/fonts/remixicon.css";
+import CaptainRiding from "./pages/CaptainRiding";
 
 const App = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
@@ -69,6 +70,14 @@ const App = () => {
               element={
                 <CaptainProtectWrapper>
                   <CaptainHome />
+                </CaptainProtectWrapper>
+              }
+            />
+            <Route
+              path="/captain-riding"
+              element={
+                <CaptainProtectWrapper>
+                  <CaptainRiding />
                 </CaptainProtectWrapper>
               }
             />
