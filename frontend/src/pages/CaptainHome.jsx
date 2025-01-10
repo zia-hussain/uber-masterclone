d81e5e891dd9450352dae72d0587007a4be7173a
zia-hussain <syedziashahgillani76@gmail.com>
2025-01-10 20:50:54 +0500

    captain almost complete !!!

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -6,11 +6,15 @@ import CaptainDetails from "../components/CaptainDetails";
 import RidePopUp from "../components/RidePopUp";
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
+import ConfirmRidePopUp from "../components/ConfirmRidePopUp";
 
 const CaptainHome = () => {
-  const [isRidePopUp, setIsRidePopUp] = useState(false);
+  const [isRidePopUp, setIsRidePopUp] = useState(true);
+  const [confirmIsRidePopUp, setConfirmIsRidePopUp] = useState(false);
+  console.log(confirmIsRidePopUp, "ConfirmIsRidePopUp");
 
   const ridePopUpRef = useRef(null);
+  const confirmRidePopUpRef = useRef(null);
 
   useEffect(() => {
     if (isRidePopUp) {
@@ -23,6 +27,18 @@ const CaptainHome = () => {
       });
     }
   }, [isRidePopUp]);
+
+  useEffect(() => {
+    if (confirmIsRidePopUp) {
+      gsap.to(confirmRidePopUpRef.current, {
+        transform: "translateY(0)",
+      });
+    } else {
+      gsap.to(confirmRidePopUpRef.current, {
+        transform: "translateY(100%)",
+      });
+    }
+  }, [confirmIsRidePopUp]);
 
   return (
     <div className="h-screen ">
@@ -45,7 +61,17 @@ const CaptainHome = () => {
         ref={ridePopUpRef}
         className={`fixed bottom-0 left-0 translate-y-full z-20 w-full bg-white shadow-lg rounded-t-2xl`}
       >
-        <RidePopUp setIsRidePopUp={setIsRidePopUp} />
+        <RidePopUp
+          setConfirmIsRidePopUp={setConfirmIsRidePopUp}
+          setIsRidePopUp={setIsRidePopUp}
+        />
+      </div>
+
+      <div
+        ref={confirmRidePopUpRef}
+        className={`fixed bottom-0 h-full left-0 translate-y-full z-20 w-full bg-white shadow-lg rounded-t-2xl`}
+      >
+        <ConfirmRidePopUp setConfirmIsRidePopUp={setConfirmIsRidePopUp} />
       </div>
     </div>
   );
