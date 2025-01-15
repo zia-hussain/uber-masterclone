3f1f425581b5e8de67dfb242608e7c1edb85663e
zia-hussain <syedziashahgillani76@gmail.com>
2025-01-15 18:17:03 +0500

    need google map api

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const captainRoutes = require("./routes/captain.routes");
+const mapsRoutes = require("./routes/maps.routes");
 
 connectToDb();
 
@@ -20,5 +21,6 @@ app.get("/", (req, res) => {
 });
 app.use("/users", userRoutes);
 app.use("/captains", captainRoutes);
+app.use("/maps", mapsRoutes);
 
 module.exports = app;
