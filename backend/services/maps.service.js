27fefba013af405fc47f35af7249eb8cdb88600e
zia-hussain <syedziashahgillani76@gmail.com>
2025-01-15 18:17:15 +0500

    need google map api

---
+++ backend/services/maps.service.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+module.exports.getAddressCoordinate = async (address) => {
+  const apiKey = process.env.GOOGLE_MAPS_API;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+    address
+  )}&key=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    if (response.data.status === "OK") {
+      const location = response.data.results[0].geometry.location;
+      return {
+        ltd: location.lat,
+        lng: location.lng,
+      };
+    } else {
+      throw new Error("Unable to fetch coordinates");
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
